a22aacbeb3a753c54ba3199ee55ea90a143b078b
chenyj <302870651@qq.com>
2023-08-03 21:47:24 +0800

    ...

--- src/watch/connect.rs
+++ src/watch/connect.rs
@@ -1,9 +1,11 @@
-use std::io::Cursor;
+use std::{io::{Cursor, BufRead}, fs::read_link};
 
 use bytes::{Buf, BytesMut};
 use tokio::{io::{AsyncReadExt, AsyncWriteExt, BufWriter}, net::TcpStream};
 
 use crate::watch::message::Message;
+
+use super::message::get_line;
 
 #[derive(Debug)]
 pub struct Connection {
@@ -22,13 +24,54 @@ impl Connection {
     pub async fn read_message(&mut self) -> Option<Message> {
         loop {
             let mut buf = Cursor::new(&self.buffer[..]);
-            // if let Some(message) = self.parse_message() {
-            //     return Some(message);
-            // }
+            let mut msg = Message::new("".to_string(), 0, vec![]);
 
-            // if 0 == self.stream.read_buf(&mut self.buffer).await.unwrap() {
-            //     return None;
-            // }
+            let x = get_line(&mut buf);
+            match x {
+                Ok(x) => {
+                    msg.cmd = String::from_utf8(x.to_vec()).unwrap();
+                },
+                Err(e) => {
+                    ()
+                }
+            }
+
+            let req = get_line(&mut buf);
+            match req {
+                Ok(req) => {
+                    //to int64
+                    msg.seq = String::from_utf8(req.to_vec()).unwrap().parse::<i64>().unwrap();
+                },
+                Err(e) => {
+                    ()
+                }
+            }
+
+            let mut n : i64 = 0;
+            let vn = get_line(&mut buf);
+            match vn {
+                Ok(vn) => {
+                    //to int64
+                    n = String::from_utf8(vn.to_vec()).unwrap().parse::<i64>().unwrap();
+                },
+                Err(e) => {
+                    ()
+                }
+            }
+
+            for _ in 0..n  {
+                let x = get_line(&mut buf);
+                match x {
+                    Ok(x) => {
+                        msg.values.push(String::from_utf8(x.to_vec()).unwrap());
+                    },
+                    Err(e) => {
+                        ()
+                    }
+                }
+            }
+
+            return Some(msg);
         }
     }
 }
--- src/watch/message.rs
+++ src/watch/message.rs
@@ -1,9 +1,10 @@
+use std::{io::{Cursor, Error}};
 
 #[derive(Debug)]
 pub struct Message {
-    cmd: String,
-    seq: i64,
-    values: Vec<String>,
+    pub cmd: String,
+    pub seq: i64,
+    pub values: Vec<String>,
 }
 
 impl Message {
@@ -14,4 +15,25 @@ impl Message {
             values,
         }
     }
+}
+
+
+// / Find a line
+pub fn get_line<'a>(src: &mut Cursor<&'a [u8]>) -> Result<&'a [u8], Error> {
+    // Scan the bytes directly
+    let start = src.position() as usize;
+    // Scan to the second to last byte
+    let end = src.get_ref().len() - 1;
+
+    for i in start..end {
+        if src.get_ref()[i] == b'\n' {
+            // We found a line, update the position to be *after* the \n
+            src.set_position((i + 1) as u64);
+
+            // Return the line
+            return Ok(&src.get_ref()[start..i]);
+        }
+    }
+
+    Err(Error::new(std::io::ErrorKind::Other, "No line found"))
 }
